57ad02347b43702c16e0db65d574876ff9ffa6ae
fukicycle <lego.sato.4135@gmail.com>
2024-11-13 16:40:46 +0900

    add files

---
+++ Fukicycle.Tool.Firebase.Realtime.Database.Extention/Models/SubscribeResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Fukicycle.Tool.Firebase.Realtime.Database.Extention.Models
+{
+    internal sealed class SubscribeResponse<T>
+    {
+        public SubscribeResponse(
+            string path,
+            T @object)
+        {
+            Path = path;
+            Object = @object;
+        }
+        public string Path { get; }
+        public T Object { get; }
+    }
+}
---
+++ Fukicycle.Tool.Firebase.Realtime.Database.Extention/WASMSubscriber.cs
@@ -0,0 +1,53 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using Firebase.Database.Streaming;
+using Fukicycle.Tool.Firebase.Realtime.Database.Extention.Models;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Newtonsoft.Json;
+
+
+namespace Fukicycle.Tool.Firebase.Realtime.Database.Extention
+{
+    public static class WASMSubscriber
+    {
+        public static async Task SubscribeForWASMAsync<T>(this ChildQuery childQuery, Action<T> onNext, Action<string> onError)
+        {
+            string url = await childQuery.BuildUrlAsync();
+            using HttpClient httpClient = new HttpClient();
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+            requestMessage.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("text/stream-event"));
+            requestMessage.SetBrowserResponseStreamingEnabled(true);
+            HttpResponseMessage responseMessage = await httpClient.SendAsync(requestMessage);
+            responseMessage.EnsureSuccessStatusCode();
+            using Stream stream = await responseMessage.Content.ReadAsStreamAsync();
+            using StreamReader reader = new StreamReader(stream);
+            _ = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    string? line = await reader.ReadLineAsync();
+                    if (string.IsNullOrEmpty(line))
+                    {
+                        continue;
+                    }
+                    if (line.StartsWith("data: "))
+                    {
+                        try
+                        {
+                            string content = line.Substring(6, line.Length - 6);
+                            SubscribeResponse<T>? response = JsonConvert.DeserializeObject<SubscribeResponse<T>>(content);
+                            if (response != null)
+                            {
+                                onNext(response.Object);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            onError(ex.Message);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
